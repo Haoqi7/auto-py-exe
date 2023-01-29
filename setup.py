0522cbae65ccd2796c73f974eb7edbe5d4f8c1ca
Haoqi7 <w00989988@gmail.com>
2023-01-29 12:48:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from auto_py_to_exe import __version__ as version
 
 setup(
     name='autopyexe',
-    version=version
+    version=version,
     url='https://github.com/Haoqi7/auto-py-exe',
     license='MIT',
     author='haoqi',
